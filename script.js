c1e70c5c35882a70833d8022c039345a680e421c
abrusc93 <abrusc93@students.rowan.edu>
2022-09-03 13:30:41 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(window).scroll(function() {
+    if ($(window).scrollTop()<=50 && $(window).width()>995) {
+      $('.navbar').css('background', 'transparent');
+    } else {
+      $('.navbar').css('background', 'rgb(62, 98, 147)');
+    }
+});
+
+$('.navbar-nav>.section-link>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+ScrollReveal().reveal('.reveal', { 
+    delay: 200
+});
+
+ScrollReveal().reveal('.reveal-delay', { 
+    delay: 600
+});
+
+ScrollReveal().reveal('.reveal-slow', { 
+    delay: 300,
+    duration: 3000,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.reveal-up', { 
+    delay: 200,
+    distance: '30px'
+});
+
+$('.menu-page *').addClass('animate__animated animate__fadeIn');
