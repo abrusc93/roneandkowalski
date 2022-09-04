f57af8a213b44cf0f5ff9ac8231be2a6048c910e
abrusc93 <abrusc93@students.rowan.edu>
2022-09-04 16:09:11 -0400

    Update
    Added business info to footer; made responsive.
    Fixed contact form width/responsiveness.

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ $('.navbar-nav>.section-link>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
 
+$(window).resize(function(){
+    if($(window).width()<995){
+     $('.form-container').removeClass('w-50');
+    }
+    if($(window).width()>995){
+        $('.form-container').addClass('w-50');
+    }
+});
+
+if($(window).width()<995){
+    $('.form-container').removeClass('w-50');
+}
+if($(window).width()>995){
+    $('.form-container').addClass('w-50');
+}
+
 ScrollReveal().reveal('.reveal', { 
     delay: 200
 });
